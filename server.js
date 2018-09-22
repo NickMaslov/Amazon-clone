b739c51b81f56fea51c0c06b028b3f8f2f7f3efd
Mykola Maslov <kategorychnost@gmail.com>
2018-09-22 15:09:27 -0400

    added layout; home, about page; added EJS engine; get routes as well

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var ejsMate = require('ejs-mate');
+
+
+var User = require('./models/user');
 
 var app = express();
 
@@ -14,15 +20,35 @@ mongoose.connect('mongodb://root:abc123@ds111963.mlab.com:11963/amazonclone', fu
 
 //Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    var name = 'Nick';
-    res.json('My name is ' + name);
+app.post('/create-user', function (req, res, next) {
+    var user = new User();
+
+    user.profile.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+
+    user.save(function (err) {
+        if (err) return next(err);
+
+        res.json('Successfully created a new user');
+
+    });
 });
 
-app.get('/catname', function (req, res) {
-    res.json('batman')
-})
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/about', function(req,res){
+    res.render('about');
+});
 
 app.listen(3000, function (err) {
     if (err) throw err;
