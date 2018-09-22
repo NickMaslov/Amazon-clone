957d45efabe7007367aa59b1a0173fa97761049c
Mykola Maslov <kategorychnost@gmail.com>
2018-09-22 14:07:24 -0400

    User schema, connected to MongoDB "

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://root:abc123@ds111963.mlab.com:11963/amazonclone', function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to the database');
+    }
+});
 
 //Middleware
 app.use(morgan('dev'));
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     var name = 'Nick';
-    res.json('My name is ' + name)
+    res.json('My name is ' + name);
 });
 
-app.get('/catname', function(req,res){
+app.get('/catname', function (req, res) {
     res.json('batman')
 })
 
